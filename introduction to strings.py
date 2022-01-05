1dc24d5b0134285e7fe58d4d18a1936a46252e4b
k-tania <97137203+k-tania@users.noreply.github.com>
2022-01-04 20:19:37 -0500

    Add files via upload
    adding test file

---
+++ introduction to strings.py
@@ -0,0 +1,22 @@
+v1_kids_song='''
+\t\tBaa, baa, black sheep,
+\t\tHave you any wool?
+\t\tYes sir, yes sir,
+\t\tThree bags full
+
+\t\tOne for the master,
+\t\tOne for the dame,
+\t\tAnd one for the little boy
+\t\tWho lives down the lane
+
+\t\tBaa, baa, black sheep,
+\t\tHave you any wool?
+\t\tYes sir, yes sir,
+\t\tThree bags full
+'''
+print(v1_kids_song)
+
+v2_kids_song='''\n\t\tBaa, baa, black sheep,\n\t\tHave you any wool?\n\t\tYes sir, yes sir,\n\t\tThree bags full\n\n\t\tOne for the master,\n\t\tOne for the dame,\n\t\tAnd one for the little boy\n\t\tWho lives down the lane\n\n\t\tBaa, baa, black sheep,\n\t\tHave you any wool?\n\t\tYes sir, yes sir,\n\t\tThree bags full'''
+
+print(v2_kids_song)
+
